4abc88d27dfae915f561416ba644f351e27f574b
mafc.monteiro <mafc.monteiro@outlook.com>
2018-12-11 17:17:04 -0200

    new itens

--- Ex-App/app-FrontEnd/src/about/about.jsx
+++ Ex-App/app-FrontEnd/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeaders from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeaders name='Sobre' small=' Nós'></PageHeaders>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dorlor sit amet</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dorlor sit amet</p>
+        <h2>Impresa</h2>
+        <p>Lorem ipsum dorlor sit amet</p>
     </div>
 )
