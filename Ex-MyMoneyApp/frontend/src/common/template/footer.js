d81813213b07f2beb5380631d63bb446d3d77726
mafc.monteiro <mafc.monteiro@outlook.com>
2019-01-11 21:21:27 -0200

    Dashboard

---
+++ Ex-MyMoneyApp/frontend/src/common/template/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            <a href='http://cod3r.com.br' target='_blank'>Cod3r</a>
+        </strong>
+    </footer>
+)
--- Ex-MyMoneyApp/frontend/src/main/app.jsx
+++ Ex-MyMoneyApp/frontend/src/main/app.jsx
@@ -2,12 +2,18 @@ import '../common/template/dependencies'
 import React from 'react'
 
 import Header from '../common/template/header'
+import Footer from '../common/template/footer'
 import Sidebar from '../common/template/sideBar'
 
+import Routes from './routes'
 
 export default props => (
     <div className='wrapper'>
         <Header />
         <Sidebar></Sidebar>
+        <div className='content-wrapper'>            
+            <Routes></Routes>
+        </div>
+        <Footer></Footer>
     </div>
 )
